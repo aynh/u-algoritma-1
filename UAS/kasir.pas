81f61737ffce33f61b2d32f8ba4a188c7f3471ee
Aynh <99479536+Aynh@users.noreply.github.com>
2023-01-10 17:28:28 +0800

    Add auto-formatted table

--- UAS/kasir.pas
+++ UAS/kasir.pas
@@ -1,7 +1,7 @@
 
 program kasir;
 
-uses crt;
+uses crt, math, strutils;
 
 type 
   tipe_barang = record
@@ -21,11 +21,17 @@ var
   input_integer: integer;
   counter: integer;
 
+  min_no_len: integer = 2;
+  min_nama_len: integer = 4;
+  min_harga_len: integer = 5;
+  min_jumlah_len: integer = 6;
+
 begin
   clrscr;
   write('jumlah barang: ');
   readln(input_integer);
 
+  min_no_len := max(min_no_len, floor(log10(input_integer)));
   setLength(barang, input_integer);
   for counter := 1 to input_integer do
     begin
@@ -35,10 +41,15 @@ begin
         begin
           write('Nama: ');
           readln(nama);
+          min_nama_len := max(min_nama_len, length(nama));
+
           write('Harga: ');
           readln(harga);
+          min_harga_len := max(min_harga_len, floor(log10(harga))+1);
+
           write('Jumlah: ');
           readln(jumlah);
+          min_harga_len := max(min_harga_len, floor(log10(jumlah)));
 
           total_harga := total_harga + (harga * jumlah);
         end;
@@ -61,10 +72,10 @@ begin
   if input_integer = 2 then
     persen_diskon := persen_diskon + 10
   else if input_integer <> 1 then
-    begin
-      writeln('ERROR: Pilih metode pembayaran 1 atau 2!');
-      exit();
-    end;
+         begin
+           writeln('ERROR: Pilih metode pembayaran 1 atau 2!');
+           exit();
+         end;
 
   diskon := total_harga * (persen_diskon / 100);
 
@@ -80,6 +91,20 @@ begin
 
   clrscr;
   writeln('OUTPUT HASIL');
+
+  writeln('|', dupestring('-', min_no_len + 2), '|', dupestring('-', min_nama_len + 2), '|',
+  dupestring('-', min_harga_len + 2), '|', dupestring('-', min_jumlah_len + 2), '|');
+  writeln('| ', 'No':min_no_len, ' | ', 'Nama':min_nama_len, ' | ', 'Harga':min_harga_len, ' | ',
+          'Jumlah':min_jumlah_len, ' |');
+  writeln('|', dupestring('-', min_no_len + 2), '|', dupestring('-', min_nama_len + 2), '|',
+  dupestring('-', min_harga_len + 2), '|', dupestring('-', min_jumlah_len + 2), '|');
+  for counter := 1 to length(barang) do
+    with barang[counter-1] do
+      writeln('| ', counter:min_no_len, ' | ', nama:min_nama_len, ' | ', harga:min_harga_len:0,
+              ' | ', jumlah:min_jumlah_len, ' |');
+  writeln('|', dupestring('-', min_no_len + 2), '|', dupestring('-', min_nama_len + 2), '|',
+  dupestring('-', min_harga_len + 2), '|', dupestring('-', min_jumlah_len + 2), '|');
+
   writeln('> Total harga : ', total_harga:0:2);
   writeln('> Total diskon: ', diskon:0:2);
   writeln('> Total bayar : ', total_bayar:0:2);
